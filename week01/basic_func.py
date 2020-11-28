f8f69350dd6d465b4c42d6606cb0866ff83a06fe
ShirleenQin <shirleen1992qin@gmail.com>
2020-11-28 09:54:57 +0800

    Add week01 codework

---
+++ week01/basic_func.py
@@ -0,0 +1,26 @@
+import inspect
+import logging
+import os
+import pathlib
+from datetime import *
+
+def record(function_name):
+    log_folder_name = "-".join(["python", datetime.now().strftime("%Y%m%d")])
+    log_name = ".".join([function_name, "log"])
+    log_path = os.path.join("/var/log", log_folder_name, log_name)
+    #log_path = log_name
+    logging.basicConfig(filename = log_path, 
+                        level = logging.DEBUG, 
+                        datefmt = "%Y-%m-%d %H:%M:%S",
+                        format = '%(asctime)s [%(levelname)s] %(message)s'
+                        )
+    logging.info("%s is being called", function_name)
+
+def print_current_time():
+    record(inspect.currentframe().f_code.co_name)
+    print("Current time is", datetime.now())
+    
+
+
+if __name__ == "__main__":
+    print_current_time()
---
+++ week01/test_caller.py
@@ -0,0 +1,3 @@
+import basic_func
+
+basic_func.print_current_time()
